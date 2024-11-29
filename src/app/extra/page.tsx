34137676edc6741fff6ef0cc3b5e8fac499b0266
Ahmed Yaqoob Dhedhi <dhedhiahmedyaqoob@gmail.com>
2024-11-29 20:43:05 +0500

    first commit

---
+++ src/app/extra/page.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'Extra',
+    description: 'Auth',
+  }
+
+export default async function ExtraPage() {
+
+   
+
+    return <h1 className="text-5xl">Extra Page!</h1>
+
+}
